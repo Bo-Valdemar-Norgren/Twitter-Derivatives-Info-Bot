d0bf337595e2261c3611c0a4ddcb3258b926878e
bono566 <bono566@ad.liu.se>
2020-06-21 16:45:11 +0200

    Refactoring

--- twitterbot.py
+++ twitterbot.py
@@ -3,6 +3,7 @@ import tweepy
 import os
 import time
 import schedule
+from datetime import datetime, timezone
 
 class FundingBot:
 	def __init__(self, tickers_lst):
@@ -24,24 +25,30 @@ class FundingBot:
 		return tweepy.API(auth)
 
 	def run(self): # Times in CEST, needs to be updated.
-		for ticker in self.dict.keys():
-			schedule.every().day.at("06:00:30").do(self.update_funding)
-			schedule.every().day.at("14:00:30").do(self.update_funding)
-			schedule.every().day.at("22:00:30").do(self.update_funding)
-
-			schedule.every().day.at("06:01").do(self.send_tweet, ticker)
-			schedule.every().day.at("14:01").do(self.send_tweet, ticker)
-			schedule.every().day.at("22:01").do(self.send_tweet, ticker)
-
+		self.schedule_funding_updates()
+		self.schedule_tweets()
 		while True:
 			schedule.run_pending()
 			time.sleep(60) # wait one minute
+			print("Running")
 
 	def update_funding(self):
 		for ticker in self.dict.keys():
 			self.dict[ticker] = get_funding(ticker)
 
-	def add_ticker(self, ticker): # TODO: Probably need to add threading to run method for this to work.
+	def schedule_funding_updates(self):
+		schedule.every().day.at("06:01").do(self.update_funding)
+		schedule.every().day.at("14:01").do(self.update_funding)
+		schedule.every().day.at("22:01").do(self.update_funding)
+
+
+	def schedule_tweets(self):
+		for ticker in self.dict.keys():
+			schedule.every().day.at("06:02").do(self.send_tweet, ticker)
+			schedule.every().day.at("14:02").do(self.send_tweet, ticker)
+			schedule.every().day.at("22:02").do(self.send_tweet, ticker)
+
+	def add_ticker(self, ticker):
 		if ticker not in self.dict:
 			try:
 				self.dict[ticker] = get_funding(ticker)
@@ -50,7 +57,7 @@ class FundingBot:
 		else:
 			print("Ticker: %s is already being tracked." % ticker)
 
-	def send_tweet(self, ticker):
+	def send_tweet(self, ticker): # TODOO: Add timestamp for tweets not to be duplicates
 		last_funding = self.dict[ticker]
 		last_funding_rate = get_funding_rate(last_funding)
 
@@ -66,7 +73,6 @@ class FundingBot:
 			message = "📉 BitMEX Funding rate for $%s is now %s%%, a decrease of %s%%" %(ticker, last_funding_rate_percentage, percentage_change)
 		
 		self.api.update_status(message)
-
 
 def get_funding(ticker, latest=True):
 	request = requests.get("https://www.bitmex.com/api/v1/funding?symbol=%s&count=2&reverse=true" % ticker)
